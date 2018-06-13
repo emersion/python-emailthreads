b0b449d85ddb702a05d5d6fad455661bff63c7ed
emersion <contact@emersion.fr>
2018-06-13 10:39:01 +0100

    Use pytest for the test runner

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .emailreview import Text, Quote, parse_blocks, match_quotes, trim_noisy_text
+from .emailreview import Thread, parse
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 -m unittest discover -v
+python3 -m pytest test
